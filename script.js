d3f58328c2f7d984f20b0cf78c0514e2effce233
Kamal Deep <86801232+Deepgautam76@users.noreply.github.com>
2025-06-11 12:57:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let button=document.getElementById("btn");
+
+button.addEventListener("click",function(){
+	let textchange=document.getElementById("para");
+	textchange.innerHTML="<h1>Enter the Metaverse</h1>";
+});
